c46cc371644a4b422479b88366b14580755ac63e
Vysakhs98 <vysakh.official98@gmail.com>
2026-06-15 13:17:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const startButton = document.getElementById("start-btn");
+
+    startButton.addEventListener("click", () => {
+
+        const characterSection =
+            document.getElementById("character-section");
+
+        if(characterSection){
+            characterSection.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+});
